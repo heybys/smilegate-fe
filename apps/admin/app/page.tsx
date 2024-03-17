264d807dca80dee065e7bd06f9c54b5fa81675ae
heybys <byn0174@gmail.com>
2024-03-17 23:07:34 +0900

    fix import

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import styles from './page.module.css';
-import { Button } from '@repo/ui/button';
+import { Button } from '@repo/ui';
 
 export default function Home() {
   return (

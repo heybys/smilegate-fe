bc7193ad5ac559c1b8c16a3f277ee0150eea0fb2
heybys <byn0174@gmail.com>
2024-03-20 05:30:28 +0900

    add env settings

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -3,6 +3,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main className={styles.main}>
+      <h3>ENV = {process.env.ENV}</h3>
       <h1>Hello, Next.js!</h1>
     </main>
   );

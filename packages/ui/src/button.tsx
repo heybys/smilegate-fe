2df05ecf4e31ae66c28e0bd7af0f6a793e0ee425
heybys <byn0174@gmail.com>
2024-03-20 11:26:34 +0900

    lint-staged test

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -8,7 +8,8 @@ interface ButtonProps {
   appName: string;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({
+                         children, className, appName }: ButtonProps) => {
   return (
     <button
       className={className}
